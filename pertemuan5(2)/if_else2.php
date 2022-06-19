71fdf3e57e32572fb160de213616e62145d8d39b
agstiranti <105537060+agstiranti@users.noreply.github.com>
2022-06-19 22:57:23 +0700

    webpromodul

---
+++ pertemuan5(2)/if_else2.php
@@ -0,0 +1,8 @@
+<?php
+$user = "ahmatim";
+$pass = " 123";
+if ($user == "achmatim" && $pass == "123"){
+} else{
+echo "Login Gagal";
+}
+?>
---
+++ pertemuan5(2)/if_var.php
@@ -0,0 +1,8 @@
+<?php
+$user = "";
+if (!isset($user)){
+	echo "Nilai variable tidak ada/belum terbentuk";
+}else{
+	echo "Variable ada";
+}
+?>
